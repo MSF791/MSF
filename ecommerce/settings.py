eccf3bf25b82553d7c6b9941af9304c35c02e94c
Your Name <your@example.com>
2024-04-02 20:04:27 -0500

    se actualiza el requeriments.txt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,9 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appEcommerce'
 ]
-
-
-
 
 
 MIDDLEWARE = [
@@ -79,15 +80,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tienda',
+#         'USER':'postgres',
+#         'PASSWORD':'1234',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tienda',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
