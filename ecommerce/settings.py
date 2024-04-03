57689aaeada34cc15bbad874b6888a0ca2e7b128
Your Name <your@example.com>
2024-04-02 21:27:06 -0500

    se agregan configuraciones para static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
